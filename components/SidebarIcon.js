b402d4fdb9e7819b788333e562f0e69440de991f
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-11 23:16:27 +0530

    add link to pages

--- components/SidebarIcon.js
+++ components/SidebarIcon.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function SidebarIcon({ icon, text }) {
   return (
-    <Link href={text === "Home" ? "/" : text}>
+    <Link href={text === "Home" ? "/" : `#${text}`}>
       <div className="sidebar-icon group">
         {icon}
         <span className="tooltip group-hover:scale-100">{text}</span>
--- pages/About.js
+++ pages/About.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <div className="page1 text-green-500 flex flex-col w-screen h-screen bg-lightBlack p-2 ">
+    <div
+      id="About"
+      className="About text-green-500 flex flex-col w-screen h-screen bg-lightBlack p-2 ">
       <div className="title text-2xl font-bold text-center">About</div>
-      <div className="page1body m-1 text-center">
+      <div className="Aboutbody m-1 text-center">
         Skills: JavaScript, React, Next.js, Solidity, Hardhat, ethers.js
       </div>
     </div>

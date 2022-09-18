c698b5fac37df39cbc5e7b035b592bf27b52d798
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-18 21:50:09 +0530

    update home page

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -5,7 +5,9 @@ export default function HomePage() {
     <div className="Home text-green-500 flex flex-col w-screen h-screen bg-darkGray p-2 ">
       <div className="title text-2xl font-bold text-center">Home</div>
       <div className="Homebody m-1 text-center">
-        Hey I am Sidharth, a passionate Blockchain Developer
+        <div className="text-lg">
+          Hey I am Sidharth, a passionate Blockchain Developer
+        </div>
       </div>
     </div>
   );

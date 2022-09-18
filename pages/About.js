e93447ee332c17a8bc34ed65f35ac458215adca4
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-18 21:52:51 +0530

    update about

--- pages/About.js
+++ pages/About.js
@@ -7,7 +7,9 @@ export default function AboutPage() {
       className="About text-green-500 flex flex-col w-screen h-screen bg-lightBlack p-2 ">
       <div className="title text-2xl font-bold text-center">About</div>
       <div className="Aboutbody m-1 text-center">
-        Skills: JavaScript, React, Next.js, Solidity, Hardhat, ethers.js
+        <div className="text-lg">
+          Skills: JavaScript, React, Next.js, Solidity, Hardhat, ethers.js
+        </div>
       </div>
     </div>
   );

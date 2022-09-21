e5b145640b706abf31ca9d1c68fe332ff0c2481e
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-21 23:01:00 +0530

    add link

--- pages/About.js
+++ pages/About.js
@@ -8,7 +8,9 @@ export default function AboutPage() {
       <div className="title text-2xl font-bold text-center">About</div>
       <div className="Aboutbody m-1 text-center">
         <div className="text-lg">
-          <a href="https://www.alexdanielson.com/">Portfolio inspiration</a>
+          <a className="underline" href="https://www.alexdanielson.com/">
+            Portfolio inspiration
+          </a>
         </div>
       </div>
     </div>

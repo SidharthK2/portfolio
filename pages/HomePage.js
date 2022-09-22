e4f6877514e346690385c518cc96a22f13d8e3d7
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-22 20:23:30 +0530

    add styling options

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -4,8 +4,8 @@ export default function HomePage() {
   return (
     <div className="Home text-green-500 flex flex-col w-screen h-screen bg-lightBlack p-2 ">
       <div className="title text-2xl font-bold text-center">Home</div>
-      <div className="Homebody m-1 text-center">
-        <div className="text-lg">Hey I am Sid</div>
+      <div className="Homebody m-1 text-center flex flex-col items-center">
+        <div className="text-lg h-fit w-fit p-2 bg-black">Hey I am Sid</div>
       </div>
     </div>
   );

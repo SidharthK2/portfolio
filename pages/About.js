d63c1214983207966d504a21ec960ff7ed183d09
sidharthK2 <sidharthkapoor.100@gmail.com>
2022-10-23 19:30:38 +0530

    update profile link

--- pages/About.js
+++ pages/About.js
@@ -8,7 +8,7 @@ export default function AboutPage() {
       <div className="title text-2xl font-bold text-center">About</div>
       <div className="Aboutbody m-1 text-center">
         <div className="text-lg">
-          <a className="underline" href="https://www.alexdanielson.com/">
+          <a className="underline" href="https://seb-graf.com/">
             Portfolio inspiration
             <a href="https://www.theodinproject.com/lessons/node-path-getting-hired-building-your-personal-website">
               Cool Portfolios
